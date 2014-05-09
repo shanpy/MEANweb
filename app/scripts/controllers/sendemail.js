491fd355b99d7fc09d2cd065df590b733c6a25bc
Pengyin Shan <shanpy901115@gmail.com>
2014-05-09 19:24:21 -0400

    check sendemail.js

--- app/scripts/controllers/sendemail.js
+++ app/scripts/controllers/sendemail.js
@@ -4,6 +4,7 @@ angular.module('webappApp')
 	.controller('SendemailCtrl', function ($scope,$http,$location) {
 		$scope.form = {};
 		$scope.sendemail = function(){
+			console.log('sendemail.js is called');
 			$http.post('/api/sendemail', $scope.form)
 				.success(function(data){
 					window.alert("Thank you very much for email!")
