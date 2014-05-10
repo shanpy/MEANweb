9ab4bcb03a4159d0abfaabe07ab58eb08d2ea190
Pengyin Shan <shanpy901115@gmail.com>
2014-05-09 20:31:59 -0400

    add id in blogs.js

--- app/scripts/controllers/sendemail.js
+++ app/scripts/controllers/sendemail.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('webappApp')
-	.controller('SendemailCtrl', function ($scope,$http,$location) {
+	.controller('SendemailCtrl', function ($scope,$http,$location $routeParams) {
 		$scope.form = {};
 		$scope.sendemail = function(){
 			console.log('$scope.form');
-			$http.post('/api/sendemail', $scope.form)
+			$http.post('/api/sendemail'+ $routeParams.id, $scope.form)
 				.success(function(data){
 					window.alert("Thank you very much for email!")
 					$location.path('/home');
