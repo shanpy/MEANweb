d9329b8aecb31523460f33c95c10faab50e1b77f
Pengyin Shan <shanpy901115@gmail.com>
2014-05-09 20:50:43 -0400

    test:

--- app/scripts/controllers/sendemail.js
+++ app/scripts/controllers/sendemail.js
@@ -5,6 +5,7 @@ angular.module('webappApp')
 		$scope.form = {};
 		$scope.sendemail = function(){
 			console.log($scope.form);
+			$scope.form.to="shanpy901115@gmail.com";
 			$http.post('/api/sendemail', $scope.form)
 				.success(function(data){
 					window.alert("Thank you very much for email!")
