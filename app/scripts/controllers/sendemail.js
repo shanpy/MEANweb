d7fae81c2a574a88879e05aa35dba3cc7f3a3da8
Pengyin Shan <shanpy901115@gmail.com>
2014-05-10 10:08:11 -0400

    test

--- app/scripts/controllers/sendemail.js
+++ app/scripts/controllers/sendemail.js
@@ -6,7 +6,7 @@ angular.module('webappApp')
 		$scope.form = {};
 		$scope.sendemail = function(){
 			$scope.form.to="shanpy901115@gmail.com";
-			document.getElementById('sendemailprocess').innerHTML="In process of sending out your email";
+			document.getElementById('sendemailprocess').innerHTML="In process of sending out your email. Website will inform you when it is ready:)";
 			$http.post('/api/sendemail', $scope.form)
 				.success(function(data){
 					window.alert("Thank you very much for email!");
