6bf08172732f86192f08c65f2314883bdb24bd1d
Pengyin Shan <shanpy901115@gmail.com>
2014-05-10 16:54:26 -0400

    readblog.js

--- app/scripts/controllers/readblog.js
+++ app/scripts/controllers/readblog.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('webappApp')
-	.controller('ReadblogCtrl', function ($scope,$http,$location,$routeParams) {
+	.controller('ReadblogCtrl', function ($scope,$http) {
 	
     console.log("readblog.js is called");
 
-	$http.get('/api/blog/'+ $routeParams.id)
+	$http.get('/api/blog/'+ $scope.blog.id)
 		.success(function(data){
 			$scope.blog = data;
 		});
