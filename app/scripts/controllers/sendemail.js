a786fb0f0c00c496ed5519cfe04913777f5f27fe
Pengyin Shan <shanpy901115@gmail.com>
2014-05-09 20:37:49 -0400

    back

--- app/scripts/controllers/sendemail.js
+++ app/scripts/controllers/sendemail.js
@@ -4,8 +4,8 @@ angular.module('webappApp')
 	.controller('SendemailCtrl', function ($scope,$http,$location $routeParams) {
 		$scope.form = {};
 		$scope.sendemail = function(){
-			console.log('$scope.form');
-			$http.post('/api/sendemail'+ $routeParams.id, $scope.form)
+			console.log($scope.form);
+			$http.post('/api/sendemail', $scope.form)
 				.success(function(data){
 					window.alert("Thank you very much for email!")
 					$location.path('/home');
