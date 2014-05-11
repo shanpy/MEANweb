34c957f2b5baae87fc3eeb56828cf1122bec0633
Pengyin Shan <shanpy901115@gmail.com>
2014-05-11 17:28:39 -0400

    test

--- app/scripts/controllers/blogindex.js
+++ app/scripts/controllers/blogindex.js
@@ -20,51 +20,41 @@ angular.module('webappApp')
 		};
 /***************************************************************/
 		$scope.readblog = function(id){
-
 			var show = document.getElementById("fullblog");
-
 			$http.get('/api/blog/'+ id)
 			.success(function(data){
 			if($scope.isCollapsed == true){
-				$scope.isCollapsed = false;
-			}
-			else{$scope.isCollapsed = true;}
 			show.innerHTML = '<center>' + '<h5>'+ data[0].title + '</h5>' + '</center>' 
 							+ '<p>' + data[0].content + '</p>' ;
-		})
+		}})
 			.error(function(err){
 			console.log(err);
+			show.innerHTML="Sorry, we can't bring full version blog now...";
+		});
+		};
+
+		$scope.checkCollapse = function(){
 			if($scope.isCollapsed == true){
 				$scope.isCollapsed = false;
 			}
 			else{$scope.isCollapsed = true;}
-			show.innerHTML="Sorry, we can't bring full version blog now...";
-		});
 		};
 /***************************************************************/
 		$scope.editblog = function(id){
 			
-		var editpart = document.getElementById('editblog');
-		editpart.innerHTML = 
-      '<input ng-model="title"/>'+
-      '<input ng-model="introduction"/>' + 
-      '<input ng-model="content"/>' +
-      '<button ng-click="doedit(blog.id)">Finish Edit</button>';
-
         $http.get('/api/blog/' + id)
             .success(function(data) {
             		console.log(data);
-            		$scope.id = data[0].id;
-                    $scope.title = data[0].title;
-                    $scope.introduction = data[0].introduction;
-                    $scope.content = data[0].content;
+                    $scope.edit.title = data[0].title;
+                    $scope.edit.introduction = data[0].introduction;
+                    $scope.edit.content = data[0].content;
                 });
 
 		};
 
 		$scope.doedit = function(id){
 				console.log("doedit is called");
-				$http.put('/api/blog/'+ id, $scope.content)
+				$http.put('/api/blog/'+ id, $scope.edit)
                 .success(function(data){
                 	console.log(data);
                 	editpart.innerHTML="";
