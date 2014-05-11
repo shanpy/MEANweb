6850f5cbc0a1b2f159d2ebdc4700e7f8d7507bd4
Pengyin Shan <shanpy901115@gmail.com>
2014-05-11 12:50:43 -0400

    test

--- app/scripts/controllers/readblog.js
+++ app/scripts/controllers/readblog.js
@@ -3,9 +3,10 @@
 angular.module('webappApp')
 	.controller('ReadblogCtrl', function ($scope,$http) {
 	
-	console.log($scope.id);
+	console.log($scope.introduction);
+	console.log($scope.blogs);
 
-	$http.get('/api/blog'+ $scope.blog.id)
+	$http.get('/api/blog0')
 		.success(function(data){
 			console.log(data);
 			$scope.blog.title= data.title;
