187cae60d7e6e1fa1598e0f17dc759434a521236
Pengyin Shan <shanpy901115@gmail.com>
2014-05-11 13:20:03 -0400

    tet

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,10 +36,11 @@ angular.module('webappApp', [
         controller: 'AddblogCtrl',
         authenticate: true
       })
+      /*
       .when('/readblog/:id', {
         templateUrl: 'partials/readblog',
         controller: 'ReadblogCtrl'
-      })
+      })*/
       .when('/editblog/:id', {
         templateUrl: 'partials/editblog',
         controller: 'EditblogCtrl',
