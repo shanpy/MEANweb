5372fb777176211d48729481f6af28f3e4274bcb
Pengyin Shan <shanpy901115@gmail.com>
2014-05-18 19:44:39 -0400

    test

--- app/scripts/controllers/blogindex.js
+++ app/scripts/controllers/blogindex.js
@@ -2,9 +2,52 @@
 
 console.log("blogindex.js is called");
 
-angular.module('webappApp').controller('IndexblogCtrl', function ($scope,$http,$rootScope,$location,$modal,$route) {
+angular.module('webappApp').controller('IndexblogCtrl', function ($scope,$http,$rootScope,$location,$modal,$route,$timeout, $animation) {
 
-/**************************Slide Show****************************/
+/********Slider********************/
+
+	$scope.ready = 'true';
+	$scope.article = '1';
+	
+	$scope.autoChanges = true;
+	$scope.time = '400';
+	$scope.easing = '0.250, 0.250, 0.750, 0.750';
+	$scope.position = 'Top';
+	$scope.scaley = '0';
+	$scope.scaleyEnabled = true;
+	$scope.scalex = '0';
+	$scope.scalexEnabled = false;
+	$scope.rotatey = '90';
+	$scope.rotateyEnabled = false;
+	$scope.rotatex = '90';
+	$scope.rotatexEnabled = true;
+	$scope.rotatez = '90';
+	$scope.rotatezEnabled = true;
+	$scope.translatex = '0';
+	$scope.translatexEnabled = false;
+	$scope.translatey = '0';
+	$scope.translateyEnabled = false;
+	$scope.translatez = '30';
+	$scope.translatezEnabled = true;
+	$scope.skewx = '45';
+	$scope.skewxEnabled = true;
+	$scope.skewy = '30';
+	$scope.skewyEnabled = true;
+
+	function doSomething() {		
+			switch($scope.article){
+			case "1":
+				$scope.article = "2";
+				break;
+			case "2":
+				$scope.article = "1";
+				break;
+			}
+		$timeout(doSomething, 1500 + getRandomInt(1000) + TryParseInt($scope.time, 1000));	  	  
+	}
+	$timeout(doSomething, 1500 + getRandomInt(1000) + TryParseInt($scope.time, 1000));
+
+/**************************Slide Show******
 		$scope.slideshowInterval = 10000;
 		var slides = $scope.slides = [];
 		$scope.addSlide = function(){
@@ -18,10 +61,10 @@ angular.module('webappApp').controller('IndexblogCtrl', function ($scope,$http,$
 				text: "This is a test photo I took when I traved to Boston."
 				})
 		};
-		for (var i=0; i<2; i++){
+		for (var i=0; i<1; i++){
 			$scope.addSlide();
 		}
-
+	**********************/
 		$http.get('/api/blogs').success(function(data){
 			$scope.blogs = data;
 		});
