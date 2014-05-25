99307c703332a5c93fd3407b097ec49c21673c84
Pengyin Shan <shanpy901115@gmail.com>
2014-05-25 19:48:40 -0400

    test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,27 +7,20 @@ angular.module('webappApp')
   	{
   		src : "./app/images/5-22.png",
   		title : "This is a test for blog update",
-  		content: "This is a test graph for blog update 1."
+  		content: "This is a test graph for blog update 1.",
+  		index : 0
 
   	},
   	{
   		src : "./app/images/template.jpg",
   		title : "This is a test for blog update",
-  		content: "This is a test graph for blog update 2."
+  		content: "This is a test graph for blog update 2.",
+  		index: 1
   	}
   	];
 
 
-	$scope.currentIndex = 0;	
-	$scope.setCurrentSlideIndex = function (index){
-		$scope.currentIndex = index;
-	};
-	$scope.isCurrentSlideIndex = function (index){
-
-		console.log(index);
-		return $scope.currentIndex === index;
-	};
-	
+	$scope.currentIndex = 0;
 
 	$scope.next = function (){
 		 $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
