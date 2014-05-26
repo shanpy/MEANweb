8dc657364874e3f52fe53c0bfbc5b27634fdce4e
Pengyin Shan <shanpy901115@gmail.com>
2014-05-25 20:02:01 -0400

    test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,34 +7,32 @@ angular.module('webappApp')
   	{
   		src : "./app/images/5-22.png",
   		title : "This is a test for blog update",
-  		content: "This is a test graph for blog update 1.",
-  		index : 0
+  		content: "This is a test graph for blog update 1."
 
   	},
   	{
   		src : "./app/images/template.jpg",
   		title : "This is a test for blog update",
-  		content: "This is a test graph for blog update 2.",
-  		index: 1
+  		content: "This is a test graph for blog update 2."
   	}
   	];
 
 
-	$scope.currentIndex = 0;
-	$scope.slides[$scope.currentIndex].visible = true;
+	$scope.currentIndex = {index:0};
+	$scope.slides[$scope.currentIndex.index].visible = true;
 	
 	$scope.next = function (){
 		$scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
 		console.log("After click: " + $scope.currentIndex);
 	};
 	$scope.prev = function (){
-		console.log("Before click: " + $scope.currentIndex);
-		if($scope.currentIndex < $scope.slides.length - 1){
-			var tmp = $scope.currentIndex;
+		console.log("Before click: " + $scope.currentIndex.index);
+		if($scope.currentIndex.index < $scope.slides.length - 1){
+			var tmp = $scope.currentIndex.index;
 			tmp++;			
 			console.log(tmp);
-			$scope.currentIndex = tmp;
-			console.log($scope.currentIndex);
+			$scope.currentIndex.index = tmp;
+			console.log($scope.currentIndex.index);
 		}
 		else{
 			$scope.currentIndex = 0;
@@ -43,11 +41,11 @@ angular.module('webappApp')
 	};
 
 
-	$scope.watch('currentIndex', function(){
+	$scope.watch('currentIndex.index', function(){
 		$scope.slides.forEach(function(slide){
 			slide.visible = false;
 		});
-		$scope.slides[$scope.currentIndex].visible = true;
+		$scope.slides[$scope.currentIndex.index].visible = true;
 	});
 
 });
